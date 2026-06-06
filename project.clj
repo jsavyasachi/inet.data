e4395963add8e4d3e5e8a915e516a57497d019c2
Savyasachi <jsavyasachi@gmail.com>
2026-06-05 21:18:41 -0700

    build: modernize toolchain (Clojure 1.12, JDK 8, hier-set 1.2.0, fork coordinate; drop ragel/codox)

--- project.clj
+++ project.clj
@@ -1,30 +1,24 @@
-(defproject com.damballa/inet.data "0.5.8-SNAPSHOT"
+(defproject net.clojars.savya/inet.data "0.6.0"
   :description "Represent and manipulate various Internet entities as data."
-  :url "http://github.com/llasram/inet.data"
+  :url "https://github.com/jsavyasachi/inet.data"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [hier-set "1.1.2"]]
-  :plugins [[lein-ragel "0.1.0"]
-            [codox "0.6.6"]]
+  :dependencies [[org.clojure/clojure "1.12.5"]
+                 [net.clojars.savya/hier-set "1.2.0"]]
   :source-paths ["src/clojure"]
-  :java-source-paths ["src/java" "target/ragel"]
-  :ragel-source-paths ["src/ragel"]
-  :javac-options ["-target" "1.7" "-source" "1.7"]
-  :prep-tasks ["ragel" "javac"]
-  :codox {:src-dir-uri "https://github.com/damballa/inet.data/blob/master/"
-          :src-linenum-anchor-prefix "L"
-          :sources ["src/clojure"]
-          :exclude [inet.data.util]
-          :output-dir "tmp/codox"}
-  :aliases {"prep" ["do" "ragel," "javac"]
-            "all" ["with-profile" ~(str "+clojure-1-6:"
-                                        "+clojure-1-7")]}
+  ;; The IP and DNS parsers under inet/data/{ip,dns} are generated from the ragel
+  ;; grammars in src/ragel/*.java.rl and committed directly, so building no
+  ;; longer requires the ragel compiler. Regenerate with `ragel -J` only if you
+  ;; edit a .rl grammar.
+  :java-source-paths ["src/java"]
+  :javac-options ["-target" "8" "-source" "8" "-Xlint:-options"]
+  :aliases {"all" ["with-profile" ~(str "+clojure-1-10:"
+                                        "+clojure-1-11:"
+                                        "+clojure-1-12")]}
   :profiles {:provided {:dependencies
                         [[byteable "0.2.0"]
                          [com.damballa/abracad "0.4.12"]]}
-             :clojure-1-6 {:dependencies
-                           [[org.clojure/clojure "1.6.0"]]}
-             :clojure-1-7 {:dependencies
-                           [[org.clojure/clojure "1.7.0"]]}})
+             :clojure-1-10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :clojure-1-11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
+             :clojure-1-12 {:dependencies [[org.clojure/clojure "1.12.5"]]}})
